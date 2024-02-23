b1cfa42932ebb67175ce2b67d3e13d382a0f0d7e
ankitaeth <ankitacode11@gmail.com>
2024-02-23 09:34:02 +0530

    added importwallet api and seedpharse api integrate

--- app/importwallet/page.jsx
+++ app/importwallet/page.jsx
@@ -1,220 +1,4 @@
 "use client";
-// import React, { useState } from "react";
-// import Link from "next/link";
-// import Image from "next/image";
-// import check from "../../assets/eoa/checkmark.png";
-// import HeaderEOA from "@/components/common/HeaderEOA";
-// import { BsArrowLeft } from "react-icons/bs";
-
-// function ImportWallet() {
-//   const secretPhrase = [
-//     { word: "", required: true },
-//     { word: "", required: true },
-//     { word: "", required: true },
-//     { word: "", required: true },
-//     { word: "", required: true },
-//     { word: "", required: true },
-//     { word: "", required: true },
-//     { word: "", required: true },
-//     { word: "", required: true },
-//     { word: "", required: true },
-//     { word: "", required: true },
-//     { word: "", required: true },
-//   ];
-
-//   const initialUserInput = Array(secretPhrase.length).fill("");
-//   const [userInput, setUserInput] = useState(initialUserInput);
-//   const [password, setPassword] = useState("");
-//   const [confirmPassword, setConfirmPassword] = useState("");
-//   const [passwordError, setPasswordError] = useState("");
-//   const [error, setError] = useState("");
-//   const [showSecretPhrase, setShowSecretPhrase] = useState(false);
-//   const [showNewPassword, setShowNewPassword] = useState(false);
-//   const [showConfirmPassword, setShowConfirmPassword] = useState(false);
-//   const [isChecked, setIsChecked] = useState(false); // Define isChecked state
-
-//   const handleInputChange = (index, value) => {
-//     const newUserInput = [...userInput];
-//     newUserInput[index] = value;
-//     setUserInput(newUserInput);
-//   };
-
-//   const handleConfirm = () => {
-//     // Check if all required fields are filled
-//     for (let i = 0; i < secretPhrase.length; i++) {
-//       if (secretPhrase[i].required && userInput[i] === "") {
-//         setError("Please fill in all required fields.");
-//         return;
-//       }
-//     }
-//     // Check if password and confirm password match
-//     if (password !== confirmPassword) {
-//       setPasswordError("Passwords do not match");
-//       return;
-//     }
-//     // Check if terms are agreed
-//     if (!isChecked) {
-//       setError("Please agree to the Terms of Use.");
-//       return;
-//     }
-
-//     setError("");
-//     setPasswordError("");
-//   };
-
-//   const toggleFieldVisibility = (fieldName) => {
-//     switch (fieldName) {
-//       case "secretPhrase":
-//         setShowSecretPhrase(!showSecretPhrase);
-//         break;
-//       case "newPassword":
-//         setShowNewPassword(!showNewPassword);
-//         break;
-//       case "confirmPassword":
-//         setShowConfirmPassword(!showConfirmPassword);
-//         break;
-//       default:
-//         break;
-//     }
-//   };
-
-//   return (
-//     <div className="h-full md:px-4 py-4 flex flex-col">
-//       <HeaderEOA title="Import Existing Wallet " className="md:hidden block" />
-//       <div className="md:w-[70%] lg:w-[45%] xl:w-[35%] container mx-auto">
-//         <hr className="mt-6 w-auto" />
-
-//         <div className="text-center mx-auto mb-12 max-w-xl">
-//           <h1 className="items-center flex md:hidden gap-2 mx-2 text-xl font-bold mt-6">
-//             {" "}
-//             <Link href="/createwallet1">
-//               <BsArrowLeft />
-//             </Link>
-//             Import Existing Wallet
-//           </h1>
-//           <div className="md:hidden flex gap-4 mb-4 ml-2 font-bold text-lg items-center "></div>
-//           <h2 className="text-xl text-center font-bold mb-4 mt-8">
-//             Import Wallet From Secret Recovery Pharse
-//           </h2>
-//           <p className="mx-auto">
-//             Write down this 12-word secret recovery phrase and password after
-//             you can access your creso wallet.
-//           </p>
-//         </div>
-//         <h2 className="my-4 max-w-md font-bold md:mb-2 mb-1 mx-4">
-//           Secret Recovery Pharse
-//           <button
-//             type="button"
-//             className="text-[#FF4085] m-2 justify-end items-end font-normal"
-//             onClick={() => toggleFieldVisibility("secretPhrase")}
-//           >
-//             {showSecretPhrase ? "Hide" : "Show"}
-//           </button>
-//         </h2>
-
-//         <div className="rounded-3xl py-3 md:py-4 border grid grid-cols-3 md:px-5 mx-3 md:mx-auto">
-//           {secretPhrase.map((item, index) => (
-//             <div
-//               key={index}
-//               className={`rounded-full border text-center text-sm md:text-base break-words  m-1 p-1 lg:p-2 md:my-1.5 ${
-//                 item.required ? "" : "bg-white"
-//               } `}
-//             >
-//               {item.required ? (
-//                 <input
-//                   type={showSecretPhrase ? "text" : "password"}
-//                   value={userInput[index]}
-//                   onChange={(e) => handleInputChange(index, e.target.value)}
-//                   className="w-full outline-none text-black placeholder-black"
-//                   placeholder={item.word}
-//                 />
-//               ) : (
-//                 <span>{item.word}</span>
-//               )}
-//             </div>
-//           ))}
-//         </div>
-//         <div className="items-center justify-center">
-//           <form className="mx-4">
-//             <div className="my-4 mx-auto max-w-md">
-//               <label
-//                 htmlFor="password"
-//                 className="block text-gray-700 font-bold md:mb-2 mb-1 items-center"
-//               >
-//                 New Password
-//                 <button
-//                   type="button"
-//                   className="text-[#FF4085] m-2 justify-end items-end font-normal"
-//                   onClick={() => toggleFieldVisibility("newPassword")}
-//                 >
-//                   {showNewPassword ? "Hide" : "Show"}
-//                 </button>
-//               </label>
-//               <input
-//                 type={showNewPassword ? "text" : "password"}
-//                 id="password"
-//                 value={password}
-//                 onChange={(e) => setPassword(e.target.value)}
-//                 className="shadow appearance-none w-full py-5 px-4 text-gray-700 leading-tight border rounded-full"
-//               />
-//             </div>
-
-//             <div className="md:mb-4 md:mt-8 my-2 mx-auto max-w-md">
-//               <label
-//                 htmlFor="confirmPassword"
-//                 className="block text-gray-700 font-bold md:mb-2 items-center"
-//               >
-//                 Confirm Password
-//                 <button
-//                   type="button"
-//                   className="text-[#FF4085] m-2 justify-end items-end font-normal"
-//                   onClick={() => toggleFieldVisibility("confirmPassword")}
-//                 >
-//                   {showConfirmPassword ? "Hide" : "Show"}
-//                 </button>
-//               </label>
-//               <input
-//                 type={showConfirmPassword ? "text" : "password"}
-//                 id="confirmPassword"
-//                 value={confirmPassword}
-//                 onChange={(e) => setConfirmPassword(e.target.value)}
-//                 className="shadow appearance-none w-full py-5 px-4 text-gray-700 leading-tight border rounded-full"
-//               />
-//             </div>
-//             {passwordError && <p className="text-red-500 ">{passwordError}</p>}
-//             {error && <p className="text-red-500 text-center ">{error}</p>}
-//             {/* Terms of Use */}
-//             <div className="flex items-center   pb-2">
-//               <button
-//                 className="rounded-full p-2 border-black focus:outline-none"
-//                 onClick={() => setIsChecked(!isChecked)}
-//               >
-//                 {isChecked ? (
-//                   <div className="w-6 h-6 rounded-full border my-2 mr-1 border-black"></div>
-//                 ) : (
-//                   <Image alt="" src={check} className="w-8 h-8 my-1 " />
-//                 )}
-//               </button>
-//               <span className="ml-2">
-//                 I agree to creso
-//                 <span className="text-[#FF4085] ml-1">Terms of Use</span>
-//               </span>
-//             </div>
-
-//             <div className="text-center  w-full rounded-full border border-black bg-white text-black hover:bg-black hover:text-white cursor-pointer">
-//               <button className="p-2.5" onClick={handleConfirm}>
-//                 Confirm
-//               </button>
-//             </div>
-//           </form>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default ImportWallet;
-
 import React, { useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
@@ -222,33 +6,21 @@ import check from "../../assets/eoa/checkmark.png";
 import HeaderEOA from "@/components/common/HeaderEOA";
 import { BsArrowLeft } from "react-icons/bs";
 import PasswordInput from "@/components/common/PasswordInput";
+import { createEOAWalletApi } from "@/clientApi/auth";
 
 function ImportWallet() {
-  const secretPhrase = [
-    { word: "", required: true },
-    { word: "", required: true },
-    { word: "", required: true },
-    { word: "", required: true },
-    { word: "", required: true },
-    { word: "", required: true },
-    { word: "", required: true },
-    { word: "", required: true },
-    { word: "", required: true },
-    { word: "", required: true },
-    { word: "", required: true },
-    { word: "", required: true },
-  ];
-
-  const initialUserInput = Array(secretPhrase.length).fill("");
-  const [userInput, setUserInput] = useState(initialUserInput);
+  const seedPhraseLength = 12;
+  const [userInput, setUserInput] = useState(Array(seedPhraseLength).fill(""));
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
   const [passwordError, setPasswordError] = useState("");
   const [error, setError] = useState("");
-  const [showSecretPhrase, setShowSecretPhrase] = useState(false);
+  const [showSeedPhrase, setShowSeedPhrase] = useState(false);
   const [showPassword, setShowPassword] = useState(false);
   const [showConfirmPassword, setShowConfirmPassword] = useState(false);
-  const [isChecked, setIsChecked] = useState(false); // Define isChecked state
+  const [isChecked, setIsChecked] = useState(false);
+  const [verificationStatus, setVerificationStatus] = useState("");
+
   const handlePasswordChange = (e) => {
     setPassword(e.target.value);
   };
@@ -257,66 +29,42 @@ function ImportWallet() {
     setConfirmPassword(e.target.value);
   };
 
-  const handlePasswordSubmit = (e) => {
-    e.preventDefault();
-    if (password !== confirmPassword) {
-      setPasswordError("Passwords do not match");
-    } else {
-      setPasswordError("");
-      // Proceed with password submission
-    }
-  };
-
-  const toggleShowPassword = () => {
-    setShowPassword(!showPassword);
-  };
-
-  const toggleShowConfirmPassword = () => {
-    setShowConfirmPassword(!showConfirmPassword);
-  };
   const handleInputChange = (index, value) => {
     const newUserInput = [...userInput];
     newUserInput[index] = value;
     setUserInput(newUserInput);
   };
 
-  const handleConfirm = () => {
-    // Check if all required fields are filled
-    for (let i = 0; i < secretPhrase.length; i++) {
-      if (secretPhrase[i].required && userInput[i] === "") {
-        setError("Please fill in all required fields.");
-        return;
+  const handleVerify = async () => {
+    try {
+      const authToken = localStorage.getItem("authToken");
+      const res = await createEOAWalletApi({ walletName: "EOA" });
+      const generatedPhrase = res?.data?.data?.seedPhrase || "";
+      const userInputPhrase = userInput.join(" ");
+      if (generatedPhrase === userInputPhrase) {
+        setVerificationStatus("Success");
+      } else {
+        setVerificationStatus("Failure");
       }
+    } catch (err) {
+      console.error("Error verifying recovery phrase:", err);
+      setVerificationStatus("Error");
     }
-    // Check if password and confirm password match
-    if (password !== confirmPassword) {
-      setPasswordError("Passwords do not match");
-      return;
-    }
-    // Check if terms are agreed
-    if (!isChecked) {
-      setError("Please agree to the Terms of Use.");
-      return;
-    }
+  };
 
-    // All fields are filled correctly, navigate to dashboard or handle password logic
-    // For now, just log the password
-    console.log("Password:", password);
-
-    // Clear password fields and errors
-    setPassword("");
-    setConfirmPassword("");
-    setPasswordError("");
-    setError("");
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    // Add your form submission logic here
+    // This function will be called when the user clicks the "Confirm" button
   };
 
   const toggleFieldVisibility = (fieldName) => {
     switch (fieldName) {
-      case "secretPhrase":
-        setShowSecretPhrase(!showSecretPhrase);
+      case "seedPhrase":
+        setShowSeedPhrase(!showSeedPhrase);
         break;
       case "newPassword":
-        setShowNewPassword(!showNewPassword);
+        setShowPassword(!showPassword);
         break;
       case "confirmPassword":
         setShowConfirmPassword(!showConfirmPassword);
@@ -335,14 +83,14 @@ function ImportWallet() {
         <div className="text-center mx-auto mb-12 max-w-xl">
           <h1 className="items-center flex md:hidden gap-2 mx-2 text-xl font-bold mt-6">
             {" "}
-            <Link href="/createwallet1">
+            <Link href="/welcome">
               <BsArrowLeft />
             </Link>
             Import Existing Wallet
           </h1>
           <div className="md:hidden flex gap-4 mb-4 ml-2 font-bold text-lg items-center "></div>
           <h2 className="text-xl text-center font-bold mb-4 mt-8">
-            Import Wallet From Secret Recovery Pharse
+            Import Wallet From Secret Recovery Phrase
           </h2>
           <p className="mx-auto">
             Write down this 12-word secret recovery phrase and password after
@@ -350,47 +98,36 @@ function ImportWallet() {
           </p>
         </div>
         <h2 className="my-4 max-w-md font-bold md:mb-2 mb-1 mx-4">
-          Secret Recovery Pharse
+          Secret Recovery Phrase
           <button
             type="button"
             className="text-[#FF4085] m-2 justify-end items-end font-normal"
-            onClick={() => toggleFieldVisibility("secretPhrase")}
+            onClick={() => toggleFieldVisibility("seedPhrase")}
           >
-            {showSecretPhrase ? "Hide" : "Show"}
+            {showSeedPhrase ? "Hide" : "Show"}
           </button>
         </h2>
 
         <div className="rounded-3xl py-3 md:py-4 border grid grid-cols-3 md:px-5 mx-3 md:mx-auto">
-          {secretPhrase.map((item, index) => (
-            <div
+          {userInput.map((value, index) => (
+            <input
               key={index}
-              className={`rounded-full border text-center text-sm md:text-base break-words  m-1 p-1 lg:p-2 md:my-1.5 ${
-                item.required ? "" : "bg-white"
-              } `}
-            >
-              {item.required ? (
-                <input
-                  type={showSecretPhrase ? "text" : "password"}
-                  value={userInput[index]}
-                  onChange={(e) => handleInputChange(index, e.target.value)}
-                  className="w-full outline-none text-black placeholder-black"
-                  placeholder={item.word}
-                />
-              ) : (
-                <span>{item.word}</span>
-              )}
-            </div>
+              type={showSeedPhrase ? "text" : "password"}
+              className="rounded-full border text-center text-sm md:text-base break-words m-1 p-1 lg:p-2"
+              value={value}
+              onChange={(e) => handleInputChange(index, e.target.value)}
+            />
           ))}
         </div>
         <div className="items-center justify-center">
-          <form className="mx-4">
+          <form className="mx-4" onSubmit={handleSubmit}>
             <div className="my-4 mx-auto max-w-md">
               <PasswordInput
                 label=" New Password"
                 value={password}
                 onChange={handlePasswordChange}
                 showPassword={showPassword}
-                onToggle={toggleShowPassword}
+                onToggle={() => setShowPassword(!showPassword)}
               />
             </div>
 
@@ -400,7 +137,7 @@ function ImportWallet() {
                 value={confirmPassword}
                 onChange={handleConfirmPasswordChange}
                 showPassword={showConfirmPassword}
-                onToggle={toggleShowConfirmPassword}
+                onToggle={() => setShowConfirmPassword(!showConfirmPassword)}
               />
             </div>
             {passwordError && <p className="text-red-500 ">{passwordError}</p>}
@@ -424,10 +161,23 @@ function ImportWallet() {
             </div>
 
             <div className="text-center  w-full rounded-full border border-black bg-white text-black hover:bg-black hover:text-white cursor-pointer">
-              <button className="p-2.5" onClick={handleConfirm}>
-                Confirm
+              <button className="p-2.5" onClick={handleVerify}>
+                Verify
               </button>
             </div>
+            {verificationStatus && (
+              <p
+                className={
+                  verificationStatus === "Success"
+                    ? "text-green-500"
+                    : "text-red-500"
+                }
+              >
+                {verificationStatus === "Success"
+                  ? "Recovery phrase verified successfully."
+                  : "Failed to verify recovery phrase."}
+              </p>
+            )}
           </form>
         </div>
       </div>
--- app/review-recovery-pharse/page.jsx
+++ app/review-recovery-pharse/page.jsx
@@ -1,165 +1,47 @@
-// "use client";
-// import React, { useState, useEffect } from "react";
-// import lockpassword1 from "../../assets/eoa/Lockpassword1.png";
-// import lock1 from "../../assets/eoa/Lock1.png";
-// import phone from "../../assets/eoa/Phone.png";
-// import CommonComponent from "@/components/common/CommonEOA";
-// import { enqueueSnackbar } from "notistack";
-// import { HiOutlineEye } from "react-icons/hi2";
-// import { createEoaWallet } from "@/clientApi/auth";
-
-// function ReviewRecovery() {
-//   const secretPhrase = [
-//     "apple",
-//     "banana",
-//     "orange",
-//     "grape",
-//     "peach",
-//     "mango",
-//     "pine",
-//     "water",
-//     "berry",
-//     "blue",
-//     "peach",
-//     "kiwi",
-//   ];
-
-//   const [revealed, setRevealed] = useState(false);
-//   const [inputWords, setInputWords] = useState(secretPhrase);
-
-//   const handleRevealClick = () => {
-//     setRevealed(!revealed);
-//   };
-
-//   const handleSendOTPMail = async () => {
-//     try {
-//       const res = await createEoaWallet({
-//         walletName: "EOA Wallet",
-//       });
-//       console.log("🚀 ~ handleSendOTPMail ~ res:", res);
-//       if (res?.status === 200) {
-//         enqueueSnackbar(`Successful email transmission`, {
-//           variant: "success",
-//         });
-//       } else {
-//       }
-//     } catch (err) {
-//       enqueueSnackbar(`Something went wrong`, {
-//         variant: "error",
-//       });
-//     } finally {
-//       // setLoading(false);
-//     }
-//   };
-//   useEffect(() => {
-//     handleSendOTPMail();
-//   }, []);
-
-//   return (
-//     <div className=" h-full md:px-4  py-4 flex flex-col ">
-//       <CommonComponent
-//         title="Create EOA Wallet"
-//         imageSrc1={lockpassword1}
-//         color1="black"
-//         hrColor1="black"
-//         borderColor1="white"
-//         imageSrc2={lock1}
-//         color2="[#D0F500]"
-//         borderColor2="black"
-//         textColor2="text-black"
-//         hrColor2="black"
-//         imageSrc3={phone}
-//         color3="white"
-//         textColor3="gray-300"
-//         borderColor3="grey-300"
-//       />
-//       <div className=" md:w-[70%] lg:w-[45%] xl:w-[35%] container mx-auto ">
-//         <hr className="mt-2 w-auto  " />
-
-//         <div className="text-center mx-auto mb-4 max-w-xl">
-//           <h2 className="text-xl text-center font-bold mb-4 mt-8">
-//             Write down your Secret Recovery Phrase
-//           </h2>
-//           <p className="mx-auto">
-//             Write down this 12-word secret recovery phrase and save it in a
-//             place that you trust and only you can access.
-//           </p>
-//         </div>
-
-//         <div
-//           className={`rounded-3xl mx-3 md:mx-auto py-3 md:py-4 border grid grid-cols-3  md:px-5 relative `}
-//         >
-//           {inputWords.map((word, index) => (
-//             <div
-//               key={index}
-//               className={`rounded-full border text-center text-sm md:text-base break-words  m-1 p-1 lg:p-2 md:my-1.5  ${
-//                 !revealed
-//                   ? "blur-sm bg-black opacity-[10%] text-white"
-//                   : "bg-[#A66CFF] border-black"
-//               }`}
-//               style={{ minWidth: "25%", textAlign: "center" }}
-//             >
-//               {/* {revealed ? `${index + 1}. ${word}` : "•••••"} */}
-//               {`${index + 1}. ${word.slice(0, 8)}`}
-//             </div>
-//           ))}
-//           {!revealed && (
-//             <div className="absolute flex flex-col justify-center items-center inset-0">
-//               {/* <Image src={""} alt="" /> */}
-//               <div className="text-4xl">
-//                 <HiOutlineEye />
-//               </div>
-//               <p className="my-4 text-sm">Make sure nobody Looking</p>
-//             </div>
-//           )}
-//         </div>
-
-//         <div className="text-center mt-20 w-full rounded-full border border-black  bg-black text-white cursor-pointer">
-//           <button className="p-2.5" onClick={handleRevealClick}>
-//             {revealed
-//               ? "Hide Secret Recovery Phrase"
-//               : "Reveal Secret Recovery Phrase"}
-//           </button>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default ReviewRecovery;
-
 "use client";
 import React, { useState, useEffect } from "react";
 import lockpassword1 from "../../assets/eoa/Lockpassword1.png";
 import lock1 from "../../assets/eoa/Lock1.png";
 import phone from "../../assets/eoa/Phone.png";
 import CommonComponent from "@/components/common/CommonEOA";
-import { enqueueSnackbar } from "notistack";
 import { HiOutlineEye } from "react-icons/hi2";
 import { createEOAWalletApi } from "@/clientApi/auth";
 
 function ReviewRecovery() {
   const [revealed, setRevealed] = useState(false);
-  const [recoveryPhrases, setRecoveryPhrases] = useState(Array(12).fill(""));
-  console.log(recoveryPhrases, "<------------------------recoveryPhrases");
+  const [recoveryPhrases, setRecoveryPhrases] = useState(
+    Array(12)
+      .fill("")
+      .map((word) => ({ word, revealed: false }))
+  );
 
   const handleRevealClick = async () => {
     if (!revealed) {
       try {
         const res = await createEOAWalletApi({
           walletName: "EOA",
-        }); // Call the API
+        });
         const { data } = res;
-        console.log(data?.data, "dssssssssssssssssss");
+        const seedPhrase = data?.data?.seedPhrase || "";
+        if (seedPhrase.length > 0) {
+          const seedPhraseArray = seedPhrase.split(" ");
 
-        setRecoveryPhrases(data?.data?.seedPhrase);
-        setRevealed(true);
-        console.log("Token:", data.token);
+          const phrasesWithRevealed = seedPhraseArray.map((word) => ({
+            word,
+            revealed: true,
+          }));
+
+          setRecoveryPhrases(phrasesWithRevealed);
+          setRevealed(true);
+          console.log("Token:", data.token);
+        } else {
+          console.error("Seed phrase is empty or not provided.");
+        }
       } catch (err) {
         console.error("Error fetching recovery phrases:", err);
       }
     } else {
-      setRevealed(false);
+      setRevealed(false); // Toggle the revealed state only if the phrase was previously revealed
     }
   };
 
@@ -197,7 +79,7 @@ function ReviewRecovery() {
         <div
           className={`rounded-3xl mx-3 md:mx-auto py-3 md:py-4 border grid grid-cols-3  md:px-5 relative `}
         >
-          {recoveryPhrases.map((word, index) => (
+          {recoveryPhrases.map((phraseObj, index) => (
             <div
               key={index}
               className={`rounded-full border text-center text-sm md:text-base break-words  m-1 p-1 lg:p-2 md:my-1.5  ${
@@ -207,12 +89,11 @@ function ReviewRecovery() {
               }`}
               style={{ minWidth: "25%", textAlign: "center" }}
             >
-              {`${index + 1}. ${word}`}
+              {`${index + 1}. ${phraseObj.word}`}
             </div>
           ))}
           {!revealed && (
             <div className="absolute flex flex-col justify-center items-center inset-0">
-              {/* <Image src={""} alt="" /> */}
               <div className="text-4xl">
                 <HiOutlineEye />
               </div>
